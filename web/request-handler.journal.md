4216a5eb733a3bedbc67268ed9e90f32715b358d
Hack Reactor Student <student@hackreactor.com>
2017-06-10 16:10:47 -0700

    Able to respond to GET requests for check

---
+++ web/request-handler.journal.md
@@ -0,0 +1,22 @@
+### Step 1: Specification
+Inputs: event listens from the server
+Outputs: sends data back to the server
+Side effects: none
+
+### Step 2: Justification
+I want to process request events properly
+
+### Step 3: Explanation
+The request handler listens for triggers from the server such a POST or GET requests and does things based on what the requests are, POST will update the DATABASE, GET will read the database, construct a body, and send data back to the client for confirmation.
+
+### Step 4: Visualization
+
+
+### Step 5: Approximation
+
+
+### Step 6: Verification
+
+
+### Step 7: Implementation
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,35 +1,45 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
-var http = require('http');
-var httphelpers = require('./http-helpers');
+var httphelpers = require('./http-helpers.js');
 
+
+
+
+
+
+// Specifications for test#1
+// Input: GET request from client at the path, "/"
+// Output: Response body which includes the data from index.html
+// Side effect: none  
 
 
 
 exports.handleRequest = function (req, res) {
-  var responseHeaders = defaultCorsHeaders;
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
+  //gather useful information about req
+    //useful info in this case will be the request url, method, header
+  var url = req.url;
+  var method = req.method;
+  var header = req.header;
+     
+  //to get method request which we're receiving, we need to write a res
+  // res.writeHead(status code, string-message, headers)
+  
+  res.writeHead(200, httphelpers.headers);
+  
+    //res should include headers, properly formatted res body 
+    // fs.readFile()
+      //we should get the res body by reading the index.html file and getting the contents of it. Make sure we have access to index.html, and other modules   
 
-  // RequestMethods
-  // on post
+  //end res
+  // res.end(put the response data in here)
+  res.end('hello');
 
-  if (method === 'GET') {
-    responseHeaders['Content-Type'] = 'application/json';
-    response.writeHead(200, responseHeaders);
-    
-    response.end('GET request came through');  
-  }
-
-  if (method === 'POST') {
-    
-  } 
-
-  req.on('error', function (err) {
-    console.log(`error: , ${err}`);
-  });
-
-  res.end(archive.paths.list);
+  // res.end(archive.paths.list);
 };
+
+
+// fs.readFile('/etc/passwd', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });

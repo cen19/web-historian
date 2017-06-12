5d88acda1f6ff4e228806f3a14a1bd5b43b4ecbb
cen19 <wilsoncen@gmail.com>
2017-06-12 02:40:41 -0700

    fixed handlers

--- web/request-handler.js
+++ web/request-handler.js
@@ -62,75 +62,80 @@ exports.handleRequest = function (req, res) {
   //gather useful information about req
     //useful info in this case will be the request url, method, header
   // var url = req.url;
-  var method = req.method;
-  var header = req.header;
+  // var method = req.method;
+  // var header = req.header;
 
    
-  //to get method request which we're receiving, we need to write a res
-  // res.writeHead(status code, string-message, headers)
-  if (method === 'GET' && req.url === '/') {
-    console.log('***');
-    var indexPath = archive.paths.siteAssets + '/index.html';
+  // //to get method request which we're receiving, we need to write a res
+  // // res.writeHead(status code, string-message, headers)
+  // if (method === 'GET' && req.url === '/') {
+  //   console.log('***');
+  //   var indexPath = archive.paths.siteAssets + '/index.html';
     
-    fs.readFile(indexPath, 'utf8', function(err, data) {
-      // console.log('in callback');
-      if (err) {
-        console.log(err);
-      } else {
-        // console.log(data);
-        res.end(data);
-      }
-    });
-    // res.writeHead(200, httphelpers.headers);
-    // res.end(httphelpers.serveAssets(res, asset, callback));
-  } else if (method === 'GET') {
-    console.log('*****');
-    // check url address to see if it matches against something in the archivedSites
+  //   fs.readFile(indexPath, 'utf8', function(err, data) {
+  //     // console.log('in callback');
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       // console.log(data);
+  //       res.end(data);
+  //     }
+  //   });
+  //   // res.writeHead(200, httphelpers.headers);
+  //   // res.end(httphelpers.serveAssets(res, asset, callback));
+  // } else if (method === 'GET') {
+  //   console.log('*****');
+  //   // check url address to see if it matches against something in the archivedSites
     
-    // have correct url path in hard-drive
-    var urlPathFile = archive.paths.archivedSites + req.url;
-    var statusCode;
-    // console.log(urlPathFile);
+  //   // have correct url path in hard-drive
+  //   var urlPathFile = archive.paths.archivedSites + req.url;
+  //   var statusCode;
+  //   // console.log(urlPathFile);
     
-    // read the file
-    fs.readFile(urlPathFile, 'utf8', function (err, data) {
-      if (err) {
-        statusCode = 404;
-        res.writeHead(statusCode, httphelpers.headers);
-        res.end();
-      } else {
-        res.end(data);
-      }
-    });    
+  //   // read the file
+  //   fs.readFile(urlPathFile, 'utf8', function (err, data) {
+  //     if (err) {
+  //       statusCode = 404;
+  //       res.writeHead(statusCode, httphelpers.headers);
+  //       res.end();
+  //     } else {
+  //       res.end(data);
+  //     }
+  //   });    
+  // }
+
+  // if (method === 'POST') {
+  //   var statusCode;
+  //   console.log('POST METHOD HAPPENING'); 
+  //   var sitesTxtPath = archive.paths.list;
+
+  //   var data = '';
+  //   req.on('data', function(chunk) {
+  //     data += chunk;
+  //   });
+  //   req.on('end', function(err) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       fs.writeFile(sitesTxtPath, data.slice(4) + '\n', 'utf8', function(err) {
+  //       // if there is an error, log it
+  //         if (err) {
+  //           console.log(err);
+  //         } else {
+  //           statusCode = 302;
+  //           // if there is no error, write the head for 302
+  //           res.writeHead(statusCode, httphelpers.headers);
+  //           res.end();
+  //         }
+  //       });
+  //     }
+  //   });
+  // }
+
+  var handler = actions[req.method];
+  if (handler) {
+    handler(req, res);
+  } else {
+    httphelpers.send404(res);
   }
-
-  if (method === 'POST') {
-    var statusCode;
-    console.log('POST METHOD HAPPENING'); 
-    var sitesTxtPath = archive.paths.list;
-
-    var data = '';
-    req.on('data', function(chunk) {
-      data += chunk;
-    });
-    req.on('end', function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        fs.writeFile(sitesTxtPath, data.slice(4) + '\n', 'utf8', function(err) {
-        // if there is an error, log it
-          if (err) {
-            console.log(err);
-          } else {
-            statusCode = 302;
-            // if there is no error, write the head for 302
-            res.writeHead(statusCode, httphelpers.headers);
-            res.end();
-          }
-        });
-      }
-    });
-  }
-
-  // res.end(archive.paths.list);
 };
